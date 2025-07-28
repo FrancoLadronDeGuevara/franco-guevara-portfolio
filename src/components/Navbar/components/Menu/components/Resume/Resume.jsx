e3530e6ceedc4fa4cc2b57bdb745c176439237b3
FrancoLadronDeGuevara <francoguevara93@gmail.com>
2025-07-28 14:36:48 -0300

    Resume updated

--- src/components/Navbar/components/Menu/components/Resume/Resume.jsx
+++ src/components/Navbar/components/Menu/components/Resume/Resume.jsx
@@ -1,25 +1,31 @@
 import { useTranslation } from "react-i18next";
-import cvIcon from "@/assets/icons/cv.png"
+import cvIcon from "@/assets/icons/cv.png";
 import CharacterButton from "../../../../../CharacterButton/CharacterButton";
 
 const Resume = ({ onCharacterUnlock }) => {
-    const { t, i18n } = useTranslation();
+  const { t, i18n } = useTranslation();
 
-    const links = {
-        es: "https://drive.google.com/file/d/1JB8W2sNtYRgB-aXuYEYGFSM-TYduSom7/view?usp=drive_link",
-        en: "https://drive.google.com/file/d/1gZCYPnjOhK6lXNt2eitg2Ow5IX_NaRJ-/view?usp=drive_link"
-    }
+  const links = {
+    es: "https://drive.google.com/file/d/1Bq06u1_Usha8KlKiH0YNl6QQ_Z8E1UpD/view?usp=drive_link",
+    en: "https://drive.google.com/file/d/1egC_tMhoBgO5_FxnkVp4CQdDPa3oqGqN/view?usp=drive_link",
+  };
 
-    return (
-        <div className="cv-container" onClick={() => window.open(links[i18n.language], "_blank")}>
-            <button className="nes-btn is-success icon-button">
-                <img src={cvIcon} width="20px" alt="curriculum vitae icon" />
-            </button>
-            <span style={{ whiteSpace: "nowrap" }}>
-                <CharacterButton hidingIn={t("navbar.downloadCV")} onCharacterUnlock={onCharacterUnlock} />
-            </span>
-        </div>
-    )
-}
+  return (
+    <div
+      className="cv-container"
+      onClick={() => window.open(links[i18n.language], "_blank")}
+    >
+      <button className="nes-btn is-success icon-button">
+        <img src={cvIcon} width="20px" alt="curriculum vitae icon" />
+      </button>
+      <span style={{ whiteSpace: "nowrap" }}>
+        <CharacterButton
+          hidingIn={t("navbar.downloadCV")}
+          onCharacterUnlock={onCharacterUnlock}
+        />
+      </span>
+    </div>
+  );
+};
 
-export default Resume
+export default Resume;
